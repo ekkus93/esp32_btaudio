c1243fb04f778d5ea520396b5fb17067b17ab6cb
Phillip Chin <ekkus93@gmail.com>
2026-07-28 08:25:54 -0700

    test(hfp): extend command manager stub for FD-13

--- esp_bt_audio_source/test/host_test/mocks/include/bt_hfp_manager_command_stub.h
+++ esp_bt_audio_source/test/host_test/mocks/include/bt_hfp_manager_command_stub.h
@@ -7,6 +7,9 @@ void mock_bt_hfp_manager_set_status(const bt_hfp_manager_status_t *status,
                                     esp_err_t result);
 void mock_bt_hfp_manager_set_stats(const bt_hfp_manager_stats_t *stats,
                                    esp_err_t result);
+void mock_bt_hfp_manager_set_diagnostics(
+    const bt_hfp_manager_diagnostics_t *diagnostics,
+    esp_err_t result);
 void mock_bt_hfp_manager_set_connect_result(esp_err_t result);
 void mock_bt_hfp_manager_set_disconnect_result(esp_err_t result);
 void mock_bt_hfp_manager_set_audio_start_result(esp_err_t result);
@@ -15,6 +18,7 @@ void mock_bt_hfp_manager_set_mode_result(esp_err_t result);
 void mock_bt_hfp_manager_set_reset_stats_result(esp_err_t result);
 unsigned mock_bt_hfp_manager_status_calls(void);
 unsigned mock_bt_hfp_manager_stats_calls(void);
+unsigned mock_bt_hfp_manager_diagnostics_calls(void);
 unsigned mock_bt_hfp_manager_connect_calls(void);
 unsigned mock_bt_hfp_manager_disconnect_calls(void);
 unsigned mock_bt_hfp_manager_audio_start_calls(void);
