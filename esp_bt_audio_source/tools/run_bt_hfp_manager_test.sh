4ff9ebc86e55cef5c0d2c4a30e21ac7e249da492
Phillip Chin <ekkus93@gmail.com>
2026-07-28 04:34:08 -0700

    test(hfp): exercise hardened FD-11 manager facade

--- esp_bt_audio_source/tools/run_bt_hfp_manager_test.sh
+++ esp_bt_audio_source/tools/run_bt_hfp_manager_test.sh
@@ -41,7 +41,7 @@ common_flags=(
         "${project_dir}/components/bt_manager/bt_duplex_state_core.c" \
         "${project_dir}/components/bt_manager/bt_duplex_state_mode.c" \
         "${project_dir}/components/bt_manager/bt_duplex_state_transitions.c" \
-        "${project_dir}/components/bt_manager/bt_hfp_manager.c" \
+        "${project_dir}/components/bt_manager/bt_hfp_manager_fd11.c" \
         "${test_dir}/mocks/bt_hfp_manager_dependencies.c" \
         "${test_dir}/test_bt_hfp_manager_cases.c" \
         "${test_dir}/test_bt_hfp_manager.c" \
