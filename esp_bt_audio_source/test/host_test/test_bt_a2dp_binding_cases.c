e869c1eed259f5bb29f7e346f4768a1a32f3b344
Phillip Chin <ekkus93@gmail.com>
2026-07-28 16:02:09 -0700

    test(a2dp): cover first binding and reconnect serials

--- esp_bt_audio_source/test/host_test/test_bt_a2dp_binding_cases.c
+++ esp_bt_audio_source/test/host_test/test_bt_a2dp_binding_cases.c
@@ -1,7 +1,6 @@
 #include "test_bluetooth_shared.h"
 
 #define A2DP_PEER_ONE "aa:bb:cc:dd:ee:ff"
-#define A2DP_PEER_TWO "11:22:33:44:55:66"
 
 static void set_connection_event(esp_a2d_cb_param_t *param,
                                  const uint8_t peer[6],
@@ -21,6 +20,76 @@ static void set_audio_event(esp_a2d_cb_param_t *param,
     memcpy(param->audio_stat.remote_bda, peer,
            sizeof(param->audio_stat.remote_bda));
     param->audio_stat.state = state;
+}
+
+void test_a2dp_first_profile_event_creates_and_binds_session(void)
+{
+    static const uint8_t peer[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    bt_events_a2dp_test_reset_binding();
+    bt_manager_set_autostart_enabled(false);
+    bt_manager_test_set_hfp_policy_status(false, NULL, 0U, ESP_OK);
+    bt_manager_test_set_hfp_policy_results(ESP_OK, ESP_OK);
+    bt_manager_test_set_hfp_profile_created_generation(7U);
+
+    esp_a2d_cb_param_t param;
+    set_connection_event(&param, peer, ESP_A2D_CONNECTION_STATE_CONNECTING);
+    bt_manager_test_invoke_a2dp_event(ESP_A2D_CONNECTION_STATE_EVT, &param);
+
+    TEST_ASSERT_EQUAL_UINT(1U,
+        bt_manager_test_get_hfp_profile_calls());
+    TEST_ASSERT_EQUAL_UINT32(0U,
+        bt_manager_test_get_last_hfp_profile_generation());
+
+    bt_events_a2dp_binding_snapshot_t binding;
+    TEST_ASSERT_EQUAL(ESP_OK,
+        bt_events_a2dp_test_get_binding(&binding));
+    TEST_ASSERT_TRUE(binding.valid);
+    TEST_ASSERT_EQUAL_STRING(A2DP_PEER_ONE, binding.peer_mac);
+    TEST_ASSERT_EQUAL_UINT32(7U, binding.last_duplex_generation);
+    TEST_ASSERT_NOT_EQUAL(0U, binding.lifecycle_serial);
+}
+
+void test_a2dp_reconnect_rotates_lifecycle_serial(void)
+{
+    static const uint8_t peer[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+    bt_events_a2dp_test_reset_binding();
+    bt_manager_set_autostart_enabled(false);
+    bt_manager_test_set_hfp_policy_results(ESP_OK, ESP_OK);
+    bt_manager_test_set_hfp_policy_status(
+        true, A2DP_PEER_ONE, 30U, ESP_OK);
+
+    esp_a2d_cb_param_t param;
+    set_connection_event(&param, peer, ESP_A2D_CONNECTION_STATE_CONNECTING);
+    bt_manager_test_invoke_a2dp_event(ESP_A2D_CONNECTION_STATE_EVT, &param);
+
+    bt_events_a2dp_binding_snapshot_t first;
+    TEST_ASSERT_EQUAL(ESP_OK,
+        bt_events_a2dp_test_get_binding(&first));
+    TEST_ASSERT_TRUE(first.valid);
+    TEST_ASSERT_EQUAL_UINT32(30U, first.last_duplex_generation);
+
+    set_connection_event(&param, peer, ESP_A2D_CONNECTION_STATE_DISCONNECTED);
+    bt_manager_test_invoke_a2dp_event(ESP_A2D_CONNECTION_STATE_EVT, &param);
+
+    bt_events_a2dp_binding_snapshot_t cleared;
+    TEST_ASSERT_EQUAL(ESP_OK,
+        bt_events_a2dp_test_get_binding(&cleared));
+    TEST_ASSERT_FALSE(cleared.valid);
+    TEST_ASSERT_EQUAL_UINT32(first.lifecycle_serial,
+                             cleared.lifecycle_serial);
+
+    bt_manager_test_set_hfp_policy_status(
+        true, A2DP_PEER_ONE, 31U, ESP_OK);
+    set_connection_event(&param, peer, ESP_A2D_CONNECTION_STATE_CONNECTING);
+    bt_manager_test_invoke_a2dp_event(ESP_A2D_CONNECTION_STATE_EVT, &param);
+
+    bt_events_a2dp_binding_snapshot_t second;
+    TEST_ASSERT_EQUAL(ESP_OK,
+        bt_events_a2dp_test_get_binding(&second));
+    TEST_ASSERT_TRUE(second.valid);
+    TEST_ASSERT_NOT_EQUAL(first.lifecycle_serial,
+                          second.lifecycle_serial);
+    TEST_ASSERT_EQUAL_UINT32(31U, second.last_duplex_generation);
 }
 
 void test_a2dp_audio_without_lifecycle_binding_is_rejected_and_counted(void)
@@ -123,5 +192,4 @@ void test_a2dp_wrong_peer_event_is_rejected_and_counted(void)
     TEST_ASSERT_TRUE(binding.valid);
     TEST_ASSERT_EQUAL_STRING(A2DP_PEER_ONE, binding.peer_mac);
     TEST_ASSERT_EQUAL_UINT64(1U, binding.wrong_peer_rejections);
-    (void)A2DP_PEER_TWO;
 }
